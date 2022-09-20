cbaf3fb748aa23fa5d479280a0e9f6945a546f15
calscoo <43020160+calscoo@users.noreply.github.com>
2022-09-20 09:48:40 -0700

    Update input.ps1

--- input.ps1
+++ input.ps1
@@ -1,11 +1,11 @@
 $input0 = $args[0]
 if ("$input0" -eq "w") {
-  ~\Applications\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
-  ~\Applications\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 17
+  ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
+  ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 17
 }
 elseif ("$input0" -eq "m") {
-  ~\Applications\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 16
-  ~\Applications\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 15
+  ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 16
+  ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 15
 }
 else {
 
