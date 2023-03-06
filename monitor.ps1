3caf036538ce6839c7a2ec1402489bf7b75aa4a7
calscoo <43020160+calscoo@users.noreply.github.com>
2023-03-06 07:50:15 -0800

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -1,9 +1,9 @@
 $input0 = $args[0]
-if ("$input0" -eq "-w") {
+if ("$input0" -eq "w") {
   ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
   ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 17
 }
-elseif ("$input0" -eq "-m") {
+elseif ("$input0" -eq "m") {
   ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 16
   ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 15
 }
